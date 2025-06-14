9bba7163ce7b29c2af176da88225d7dcdea360c6
cyber-scribe <vd297520@gmail.com>
2025-06-14 22:45:54 +0530

    changes

--- words.js
+++ words.js
@@ -1,39 +1,39 @@
 let words = [
     {
-        word: "addition",
-        hint: "The process of adding numbers"
+    word: "addition",
+    hint: "The process of adding numbers"
     },
     {
-        word: "meeting",
-        hint: "Event in which people come together"
+    word: "meeting",
+    hint: "Event in which people come together"
     },
     {
-        word: "number",
-        hint: "Math symbol used for counting"
+    word: "number",
+    hint: "Math symbol used for counting"
     },
     {
-        word: "exchange",
-        hint: "The act of trading"
+    word: "exchange",
+    hint: "The act of trading"
     },
     {
-        word: "canvas",
-        hint: "Piece of fabric for oil painting"
+    word: "canvas",
+    hint: "Piece of fabric for oil painting"
     },
     {
-        word: "garden",
-        hint: "Space for planting flower and plant"
+    word: "garden",
+    hint: "Space for planting flower and plant"
     },
     {
-        word: "position",
-        hint: "Location of someone or something"
+    word: "position",
+    hint: "Location of someone or something"
     },
     {
-        word: "feather",
-        hint: "Hair like outer covering of bird"
+    word: "feather",
+    hint: "Hair like outer covering of bird"
     },
     {
-        word: "comfort",
-        hint: "A pleasant feeling of relaxation"
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation"
     },
     {
     word: "tongue",
